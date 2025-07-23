13f2849891606413e1b36cf703780424f157bf91
skmitcoe <sumitkumar.mit@gmail.com>
2025-07-23 20:24:24 +0530

    audio

--- StreamlitUI.py
+++ StreamlitUI.py
@@ -1,12 +1,28 @@
 import streamlit as st
 import speech_recognition as sr
 from googletrans import Translator
+from flask import Flask, request, jsonify
 
-async def translate(text, dest_lang="en"):
+app = Flask(__name__)
+
+
+@app.route("/home")
+def getname():
+    return "This is backend in python"
+
+
+@app.route("/translate", methods=["POST"])
+async def translate():
+    input_params = request.get_json()
+    text = input_params["text"]
+    print("def translate(): param text: ", text)
+    dest_lang = input_params["dest_lang"]
+    print("def translate(): param dest_lang: ", dest_lang)
     try:
         translator = Translator()
         print("def translate(): input text: ", text)
         translation = await translator.translate(text, dest=dest_lang)
+        print("Translated json: ", translation)
         translated_text = translation.text
         print("def translate(): translated text: ", translated_text)
         return translated_text
@@ -18,12 +34,10 @@ async def translate(text, dest_lang="en"):
 st.title(":rainbow[Sumit Kumar's AI Space]")
 st.write("My web page to explore Natural Language Processing")
 
-if st.button('Generate'):
-    st.write("This is the generated text on click of Generate button")
-
 audio_file = st.audio_input("Record your voice message")
 r = sr.Recognizer()
 
+st.write("or")
 
 input_text = st.text_input(label="Enter your text")
 
@@ -45,24 +59,30 @@ if audio_file:
         except sr.RequestError as e:
             print(f"Could not request results from Google Speech Recognition service; {e}")
 
-async def main():
-    if st.button('Translate to Marathi'):
-        translated_text = await translate(input_text, dest_lang="mr")
-        st.write(translated_text)
+def post_call(input_text, dest_lang_code):
+    with app.test_client() as client:
+        response = client.post('/translate',json={'text': input_text, 'dest_lang': dest_lang_code})
+        return response.text
 
-    if st.button('Translate to Hindi'):
-        translated_text = await translate(input_text, dest_lang="hi")
-        st.write(translated_text)
+dest_lang_code = st.text_input(label="Enter destination language")
+if st.button('Translate to input language'):
+    translated_text = post_call(input_text, "kn")
+    st.write(translated_text)
 
-    if st.button('Translate to Kannada'):
-        translated_text = await translate(input_text, dest_lang="kn")
-        st.write(translated_text)
+if st.button('Translate to Marathi'):
+    translated_text = post_call(input_text, "mr")
+    st.write(translated_text)
 
-if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+if st.button('Translate to Hindi'):
+    translated_text = post_call(input_text, "hi")
+    st.write(translated_text)
 
-select_option = st.selectbox('What is you favourite color?', ("Red", "Blue", "Green"), index=None, placeholder="Enter your favourite color")
+if st.button('Translate to Kannada'):
+    translated_text = post_call(input_text, "kn")
+    st.write(translated_text)
+
+select_option = st.selectbox('What is you favourite color?', ("Red", "Blue", "Green"), index=None,
+                             placeholder="Enter your favourite color")
 if select_option:
     st.write('Your favourite color is:', select_option)
 
@@ -70,6 +90,12 @@ radio_option = st.radio("What is you favourite movie genre?", ("Comedy", "Drama"
 if radio_option:
     st.write('Your favourite movie genre is:', radio_option)
 
-multiselect_option = st.multiselect("What are your favourite colors?", ("Red", "Blue", "Green", "Yellow"), placeholder="Enter your favourite color(s)")
+multiselect_option = st.multiselect("What are your favourite colors?", ("Red", "Blue", "Green", "Yellow"),
+                                    placeholder="Enter your favourite color(s)")
 if multiselect_option:
     st.write('You selected:', multiselect_option)
+
+if __name__ == "__main__":
+    app.run()
+    # import asyncio
+    # asyncio.run(main())
